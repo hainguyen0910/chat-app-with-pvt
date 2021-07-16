16593b9e394552d85981b1e32a57a0821947d9e8
Phan Viet Tan <tanphan6061@gmail.com>
2021-07-16 22:39:09 +0700

    fix info members and add api get room by id, leave room

--- backend/controllers/room.controller.js
+++ backend/controllers/room.controller.js
@@ -1,4 +1,5 @@
 const status = require('http-status');
+const { indexOf } = require('lodash');
 const shortId = require('shortid');
 const Room = require('../models/Room');
 
@@ -9,7 +10,9 @@ shortId.characters(
 module.exports.get = async (req, res) => {
   const rooms = await Room.find({
     members: { $in: [req.userId] },
-  }).sort({ updatedAt: -1 });
+  })
+    .sort({ updatedAt: -1 })
+    .populate('members', 'fullname username sex avatar birthday _id');
 
   return res.json({ rooms });
 };
@@ -22,11 +25,16 @@ module.exports.create = async (req, res) => {
     if (!checkRandomId) break;
   }
   try {
-    const room = await Room.create({
+    let room = await Room.create({
       name,
       members: [req.userId],
       roomId: shortId.generate(),
     });
+
+    room = await Room.findById(room._id).populate(
+      'members',
+      'fullname username sex avatar birthday _id'
+    );
 
     return res.json({ room });
   } catch (err) {
@@ -40,15 +48,87 @@ module.exports.join = async (req, res) => {
   const { roomId } = req.body;
 
   try {
-    const room = await Room.findOne({ roomId });
+    let room = await Room.findOne({ roomId });
     if (!room) {
       return res.status(status.NOT_FOUND).json({
         message: 'Room not found',
       });
     }
+
+    if (room.members.includes(req.userId)) {
+      return res.status(status.BAD_REQUEST).json({
+        message: 'You have joined this room!',
+      });
+    }
+
     room.set({ members: [...room.members, req.userId] });
     await room.save();
 
+    room = await Room.findOne({ roomId }).populate(
+      'members',
+      'fullname username sex avatar birthday _id'
+    );
+
+    return res.json({ room });
+  } catch (err) {
+    return res.status(status.INTERNAL_SERVER_ERROR).json({
+      message: err.message,
+    });
+  }
+};
+
+module.exports.leave = async (req, res) => {
+  const { roomId } = req.params;
+  try {
+    let room = await Room.findOne({ roomId });
+    if (!room) {
+      return res.status(status.NOT_FOUND).json({
+        message: 'Room not found',
+      });
+    }
+
+    const indexOfCurrentUser = room.members.findIndex(
+      (member) => member == req.userId
+    );
+    if (indexOfCurrentUser === -1) {
+      return res.status(status.BAD_REQUEST).json({
+        message: "You haven't joined this room!",
+      });
+    }
+    room.set({
+      members: [
+        ...room.members.splice(0, indexOfCurrentUser),
+        ...room.members.splice(indexOfCurrentUser + 1),
+      ],
+    });
+    await room.save();
+
+    room = await Room.findOne({ roomId }).populate(
+      'members',
+      'fullname username sex avatar birthday _id'
+    );
+
+    return res.json({ room });
+  } catch (err) {
+    return res.status(status.INTERNAL_SERVER_ERROR).json({
+      message: err.message,
+    });
+  }
+};
+
+module.exports.getById = async (req, res) => {
+  const { roomId } = req.params;
+  try {
+    const room = await Room.findOne({ roomId }).populate(
+      'members',
+      'fullname username sex avatar birthday _id'
+    );
+
+    if (!room) {
+      return res.status(status.NOT_FOUND).json({
+        message: 'Room not found',
+      });
+    }
     return res.json({ room });
   } catch (err) {
     return res.status(status.INTERNAL_SERVER_ERROR).json({
